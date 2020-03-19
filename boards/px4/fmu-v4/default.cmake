72956474f52cd56fe9c225f316262748c47b0eed
Daniel Agar <daniel@agar.ca>
2020-03-19 11:06:27 -0400

    [DO NOT MERGE] px4_fmu-v5, px4_fmu-v5x, modalai_fv-v1 switch to new InvenSense drivers

--- boards/px4/fmu-v4/default.cmake
+++ boards/px4/fmu-v4/default.cmake
@@ -30,7 +30,7 @@ px4_add_board(
 		imu/adis16497
 		imu/invensense/icm20602
 		imu/invensense/icm20608g
-		#imu/invensense/mpu9250
+		imu/invensense/mpu9250
 		#imu/mpu6000 # legacy icm20602/icm20608g driver
 		imu/mpu9250
 		irlock
